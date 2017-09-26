8758a22a9b90e93b09916c88c70f469221bb8598
Moraru Ion <morarou.y@gmail.com>
2017-09-26 18:03:29 +0200

    Structure the game

---
+++ src/containers/Board.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Square from '../components/Square';
+
+class Board extends Component {
+    constructor() {
+      super();
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+
+    renderSquare(i) {
+      return <Square value={this.state.squares[i]} />;
+    }
+  
+    render() {
+      const status = 'Next player: X';
+  
+      return (
+        <div>
+          <div className="status">{status}</div>
+          <div className="board-row">
+            {this.renderSquare(0)}
+            {this.renderSquare(1)}
+            {this.renderSquare(2)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(3)}
+            {this.renderSquare(4)}
+            {this.renderSquare(5)}
+          </div>
+          <div className="board-row">
+            {this.renderSquare(6)}
+            {this.renderSquare(7)}
+            {this.renderSquare(8)}
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default Board;
